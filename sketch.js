a40322fa74c7fc7d2d626e81b14d6c6afa8e268e
utkarshkank <80094801+utkarshkank@users.noreply.github.com>
2021-10-28 18:03:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var br;
+var man;
+var pest;
+var pesticide;
+
+var background, man1, pest1, poison;
+
+function preload(){
+
+br = loadImage("images/house.jpg");
+man = loadImage("images/man.png");
+pest = loadImage("images/pest.png");
+pesticide = loadImage("images/pesticide.png");
+
+}
+
+function setup(){
+createCanvas(windowWidth,windowHeight);
+background = createSprite(windowWidth/2,windowHeight/2,50,50);
+background.addImage(br);
+man1 = createSprite(windowWidth-20,windowHeight-20,50,50);
+man1.addImage(man);
+pest1 = createSprite(windowWidth+20,windowHeight+20,50,50);
+pest1.addImage(pest);
+poison = createSprite(200,200,50,50);
+poison.addImage(pesticide);
+
+
+}
+
+function draw(){
+background(0)
+
+
+drawSprites()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
